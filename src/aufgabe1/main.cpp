4020a5d0eeaac09cc5de274cbd8f592be2140404
Christoph Weiler <christoph.weiler@yahoo.de>
2015-10-25 00:11:39 +0200

    Added argument parsing

--- src/aufgabe1/main.cpp
+++ src/aufgabe1/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <memory>
+#include <unistd.h> // Getopt from C Library
 #include "kpmp_instance.h"
 #include "kpmp_solution.h"
 #include "kpmp_solution_writer.h"
@@ -7,14 +8,57 @@
 #include "greedyconstruction.h"
 #include "utils.h"
 
-int main()
+void usage() {
+	std::cout << "Usage: ./heuOpt -c {g | r} [-r]" << std::endl;
+	std::cout << "-c: Build with construction heuristic " << std::endl;
+	std::cout << "    option g: Greedy construction heuristic" << std::endl;
+	std::cout << "    option r: Rating construction heuristic" << std::endl;
+	std::cout << "-r: Randomize construction heuristic" << std::endl;
+}
+
+int main(int argc, char** argv)
 {	
-	GreedyConstruction gc(true);
+	// Parse arguments
+	int opt;
+	int opterr;
+	bool doConstructionHeuristic = false;
+	bool randomize = false;
+	char typeOfConstructionHeuristic;
+
+	while((opt = getopt(argc, argv, "c:r")) != -1) {
+		switch(opt) {
+			case 'c':
+				doConstructionHeuristic = true;
+				typeOfConstructionHeuristic = optarg[0];
+				break;
+			case 'r':
+				randomize = true;
+				break;
+			default:
+				usage();
+				return -1;
+		}		
+	}
+
+	if(!doConstructionHeuristic) {
+		usage();
+		return -1;
+	}
+	
+	std::shared_ptr<ConstructionHeuristic> constructor;
+	if(typeOfConstructionHeuristic == 'g') {
+		constructor = std::shared_ptr<ConstructionHeuristic>(new GreedyConstruction(randomize));
+	} else if(typeOfConstructionHeuristic == 'r') {
+		//TODO: Build in rating construction
+	} else {
+		usage();
+		return -1;
+	}
 
 	for (char i = 1; i <= 10; i++) {
 		std::string instanceName("instances/automatic-" + std::to_string(i) + ".txt");
 
-		const auto& sol = gc.construct(instanceName);
+		const auto& sol = constructor->construct(instanceName);
 
 
 		// write solution
